64d8bb24f6e297fc88f313c0c360e5d5ab606b4a
Michel Henrique Hoekstra <michel.h.hoekstra@gmail.com>
2022-06-01 21:49:54 -0300

    feat: added the freezed dependency; created the Failure and Paginated models

---
+++ lib/domain/models/paginated.dart
@@ -0,0 +1,26 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'paginated.freezed.dart';
+part 'paginated.g.dart';
+
+@freezed
+@JsonSerializable(genericArgumentFactories: true)
+class Paginated<T extends Object?> with _$Paginated<T> {
+  factory Paginated({
+    required List<T> docs,
+    required int totalDocs,
+    required int limit,
+    required int totalPages,
+    required int page,
+    required int pagingCounter,
+    required bool hasPrevPage,
+    required bool hasNextPage,
+    required int prevPage,
+    required int nextPage,
+  }) = _Paginated;
+
+  factory Paginated.fromJson(
+      Map<String, dynamic> json, T Function(Object? json) fromJsonT) {
+    return _$PaginatedFromJson<T>(json, fromJsonT);
+  }
+}
---
+++ lib/domain/models/paginated.g.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'paginated.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Paginated<T> _$PaginatedFromJson<T extends Object?>(
+  Map<String, dynamic> json,
+  T Function(Object? json) fromJsonT,
+) =>
+    Paginated<T>(
+      docs: (json['docs'] as List<dynamic>).map(fromJsonT).toList(),
+      totalDocs: json['totalDocs'] as int,
+      limit: json['limit'] as int,
+      totalPages: json['totalPages'] as int,
+      page: json['page'] as int,
+      pagingCounter: json['pagingCounter'] as int,
+      hasPrevPage: json['hasPrevPage'] as bool,
+      hasNextPage: json['hasNextPage'] as bool,
+      prevPage: json['prevPage'] as int,
+      nextPage: json['nextPage'] as int,
+    );
+
+Map<String, dynamic> _$PaginatedToJson<T extends Object?>(
+  Paginated<T> instance,
+  Object? Function(T value) toJsonT,
+) =>
+    <String, dynamic>{
+      'docs': instance.docs.map(toJsonT).toList(),
+      'totalDocs': instance.totalDocs,
+      'limit': instance.limit,
+      'totalPages': instance.totalPages,
+      'page': instance.page,
+      'pagingCounter': instance.pagingCounter,
+      'hasPrevPage': instance.hasPrevPage,
+      'hasNextPage': instance.hasNextPage,
+      'prevPage': instance.prevPage,
+      'nextPage': instance.nextPage,
+    };
